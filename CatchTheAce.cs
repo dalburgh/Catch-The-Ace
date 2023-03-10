124545cb5b804110a8eed5c13ba429743e5884b6
Anthony Silveri <a.silveri97@outlook.com>
2023-03-10 13:54:38 -0500

    Moved Validation of user input to GetYears Method

--- CatchTheAce.cs
+++ CatchTheAce.cs
@@ -20,6 +20,7 @@ namespace CatchTheAceSimulator
         // Application runtime
         public void Run(bool diagnosticsBool = false, bool logsBool = false)
         {
+            // Introduction
             Console.WriteLine("\nWelcome to the Catch the Ace Simulation!\n");
 
             Console.WriteLine("This program will simulate the Catch the Ace game for a given number of years");
@@ -28,12 +29,8 @@ namespace CatchTheAceSimulator
             // Create an unshuffled deck of cards
             int[] numberedDeck = CreateUnshuffledDeck();
 
-            int yearsToSimulate = 0;
-            // Get the number of years to simulate from the user. If the user enters a number less than 1, the user will be reprompted.
-            while (yearsToSimulate == 0)
-            {
-                yearsToSimulate = getYears();
-            };
+            // Get the number of years to simulate from the user
+            int yearsToSimulate = getYears();
 
             // Start the diagnostic stopwatch
             var watch = System.Diagnostics.Stopwatch.StartNew();
@@ -47,7 +44,7 @@ namespace CatchTheAceSimulator
             // Calculate the win percentage
             float winPercentage = (float)wins / yearsToSimulate * 100;
             Console.WriteLine($"Win Percentage:            {winPercentage}%");
-            
+
             // If the user passed in the argument "true", print simulation diagnostics
             if (diagnosticsBool)
             {
@@ -58,6 +55,7 @@ namespace CatchTheAceSimulator
                 Console.WriteLine($"Program Execution Time:    {elapsedMs} ms");
             }
 
+            // Exit the program 
             Console.Write($"Press any key to exit.");
             Console.ReadKey();
             System.Environment.Exit(0);
@@ -74,32 +72,30 @@ namespace CatchTheAceSimulator
         //  int yearsToSimulate: the number of years to simulate
         int getYears()
         {
-            try
+            int answer = 0;
+            while (answer <= 0)
             {
-                Console.Write("How many years would you like to run the simulation? : ");
-                int answer = int.Parse(Console.ReadLine());
-                if (answer > 0)
+                try
                 {
-                    return answer;
+                    Console.Write("How many years would you like to run the simulation? : ");
+                    answer = int.Parse(Console.ReadLine());
+                    if (answer <= 0)
+                    {
+                        PrintColoredLine("Please enter a number greater than 0.", ConsoleColor.Red);
+                    }
                 }
-                else
+                catch (FormatException)
                 {
-                    PrintColoredLine("Please enter a number greater than 0.", ConsoleColor.Red);
-                    return 0;
+                    PrintColoredLine("Please enter a valid number.", ConsoleColor.Red);
+                }
+                catch (Exception e)
+                {
+                    PrintColoredLine("An unknown error has occured, please try running the program again.", ConsoleColor.Red);
+                    PrintColoredLine(e.Message, ConsoleColor.Red);
+                    System.Environment.Exit(1);
                 }
             }
-            catch (FormatException)
-            {
-                PrintColoredLine("Please enter a valid number.", ConsoleColor.Red);
-                return 0;
-            }
-            catch (Exception e)
-            {
-                PrintColoredLine("An unknown error has occured, please try running the program again.", ConsoleColor.Red);
-                PrintColoredLine(e.Message, ConsoleColor.Red);
-                System.Environment.Exit(1);
-                return 0;
-            }
+            return answer;
         }
 
         // Simulates the Catch the Ace game for a given number of years
